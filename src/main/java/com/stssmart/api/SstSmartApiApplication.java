8c51ffcc5fad08dfad405d012aab37878249dd97
carlaospa <carlaospa@gmail.com>
2019-01-19 13:07:57 -0200

    primeiro

---
+++ src/main/java/com/stssmart/api/SstSmartApiApplication.java
@@ -0,0 +1,14 @@
+package com.stssmart.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstSmartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SstSmartApiApplication.class, args);
+	}
+
+}
+
